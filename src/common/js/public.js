fa0a1a4b4992d37f4afb951796a0798e95bbb6bf
zhangzhenchang <591781589@qq.com>
2018-07-03 15:50:57 +0800

    登录

--- src/common/js/public.js
+++ src/common/js/public.js
@@ -1,4 +1,4 @@
-var baseURL="http://wallet-api-test.launchain.org:50000/exchange";
+var baseURL="http://wallet-api-test.launchain.org:50000";
 var cardURL="http://wallet-api-test.launchain.org:50000";
 export {baseURL,cardURL}
 
